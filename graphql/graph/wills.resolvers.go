bfd68702a25930682b4e8545156f0f5396507988
Nikola Petrov <koliodev@protonmail.com>
2021-03-16 21:12:55 +0200

    commit migrations

---
+++ graphql/graph/wills.resolvers.go
@@ -0,0 +1,19 @@
+package graph
+
+// This file will be automatically regenerated based on the schema, any resolver implementations
+// will be copied through when generating and any unknown code will be moved to the end.
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/kolioDev/after_life/graphql/graph/model"
+)
+
+func (r *mutationResolver) CreateWill(ctx context.Context, willInput model.WillInput) (*model.Will, error) {
+	panic(fmt.Errorf("not implemented"))
+}
+
+func (r *queryResolver) Will(ctx context.Context) ([]*model.Will, error) {
+	panic(fmt.Errorf("not implemented"))
+}
