2ada8b30710698fbdf13d74af2ca717695dbd160
Nikola Petrov <koliodev@protonmail.com>
2021-03-20 23:35:40 +0200

    Revert "graphql expirement"
    This reverts commit 6e9b7a4330ae804dc94a740b6db71e3d9c508652.

--- graphql/graph/schema.resolvers.go
+++ graphql/graph/schema.resolvers.go
@@ -7,8 +7,8 @@ import (
 	"context"
 	"fmt"
 
-	"github.com/kolioDev//after_life/graphql/graph/generated"
-	"github.com/kolioDev//after_life/graphql/graph/model"
+	"github.com/kolioDev/gqlgen-todos/graph/generated"
+	"github.com/kolioDev/gqlgen-todos/graph/model"
 )
 
 func (r *mutationResolver) CreateTodo(ctx context.Context, input model.NewTodo) (*model.Todo, error) {
